0364147fe66ec8fd063be2dd7c340e6ce6177c50
Rafael Ferreira <rafaelmfer@hotmail.com>
2022-10-14 09:38:07 -0300

    :lipstick: feat: design da tela e carregamento de informações

--- app/src/main/java/com/rafaelmfer/githubrepo/presentation/HomeActivity.kt
+++ app/src/main/java/com/rafaelmfer/githubrepo/presentation/HomeActivity.kt
@@ -1,17 +1,26 @@
 package com.rafaelmfer.githubrepo.presentation
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rafaelmfer.githubrepo.R
 import com.rafaelmfer.githubrepo.data.repository.State
 import com.rafaelmfer.githubrepo.databinding.ActivityHomeBinding
 import com.rafaelmfer.githubrepo.domain.model.GitHubRepositoriesModel
+import com.rafaelmfer.githubrepo.extensions.gone
 import com.rafaelmfer.githubrepo.extensions.viewBinding
+import com.rafaelmfer.githubrepo.extensions.visible
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<HomeViewModel>()
     private val binding by viewBinding(ActivityHomeBinding::inflate)
+
+    private val reposAdapter = ReposAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,8 @@ class HomeActivity : AppCompatActivity() {
 
     private fun ActivityHomeBinding.onViewCreated() {
         observables()
+        setupToolbar()
+        setupRecycler()
     }
 
     private fun ActivityHomeBinding.observables() {
@@ -29,13 +40,52 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    private fun ActivityHomeBinding.setupToolbar() {
+        mtbRepos.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.day_night -> {
+                    when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                        Configuration.UI_MODE_NIGHT_YES -> {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        }
+                        Configuration.UI_MODE_NIGHT_NO -> {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        }
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun ActivityHomeBinding.setupRecycler() {
+        rvRepos.apply {
+            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+            adapter = reposAdapter
+        }
+    }
+
     private fun ActivityHomeBinding.handlerReposState(state: State<GitHubRepositoriesModel>) {
         when (state) {
             is State.Loading -> {
+                pbRepos.visible
+                rvRepos.gone
+                tvReposError.gone
             }
             is State.Success -> {
+                rvRepos.visible
+                reposAdapter.updateRepoList(state.model.items)
+                pbRepos.gone
+                tvReposError.gone
             }
             is State.Error -> {
+                pbRepos.gone
+                rvRepos.gone
+                tvReposError.apply {
+                    visible
+                    if (!state.message.isNullOrBlank()) text = state.message
+                }
             }
         }
     }
---
+++ app/src/main/java/com/rafaelmfer/githubrepo/presentation/ReposAdapter.kt
@@ -0,0 +1,79 @@
+package com.rafaelmfer.githubrepo.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rafaelmfer.githubrepo.databinding.ItemRepoInfoBinding
+import com.rafaelmfer.githubrepo.domain.model.ItemModel
+
+class ReposAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val repoList: MutableList<ItemModel> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ReposViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as? ReposViewHolder)?.bind(repoList[position])
+    }
+
+    override fun getItemCount(): Int = repoList.size
+
+    class ReposViewHolder(private val binding: ItemRepoInfoBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup): ReposViewHolder {
+                val view = ItemRepoInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return ReposViewHolder(view)
+            }
+        }
+
+        fun bind(item: ItemModel) {
+            binding.apply {
+                tvRepoName.text = item.repoName
+                tvRepoDescription.text = item.description
+                tvUserName.text = item.userName
+                tvStarsNumber.text = item.stargazersCount.toString()
+                tvForksNumber.text = item.forksCount.toString()
+
+                Glide.with(ivUserPhoto.context)
+                    .load(item.userPhoto)
+                    .into(ivUserPhoto)
+            }
+        }
+    }
+
+    class ReposDiffUtil(
+        private val oldList: List<ItemModel>,
+        private val newList: List<ItemModel>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].id == newList[newItemPosition].id
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return (oldList[oldItemPosition].repoName == newList[newItemPosition].repoName &&
+                    oldList[oldItemPosition].htmlUrl == newList[newItemPosition].htmlUrl)
+        }
+    }
+
+    fun updateRepoList(newRepoList: List<ItemModel>) {
+        val diffUtilCallback = ReposDiffUtil(
+            oldList = repoList,
+            newList = newRepoList
+        )
+        val diffResult = DiffUtil.calculateDiff(diffUtilCallback)
+
+        repoList.clear()
+        repoList.addAll(newRepoList)
+        diffResult.dispatchUpdatesTo(this)
+    }
+}
